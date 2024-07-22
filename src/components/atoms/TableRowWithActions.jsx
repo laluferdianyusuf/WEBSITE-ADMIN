ad828a88d30c2fc3275ad84318293e0deb88ccd4
Saputra <saputra.uta50@gmail.com>
2024-07-22 16:32:22 +0800

    Table with actions

---
+++ src/components/atoms/TableRowWithActions.jsx
@@ -0,0 +1,44 @@
+import ActionButton from "./ActionButton";
+import PropTypes from "prop-types";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+export default function TableRowWithAction({
+  data,
+  onUpdate,
+  onDelete,
+  index,
+}) {
+  const rowClass = index % 2 === 0 ? "bg-white" : "bg-custom-blue-1";
+  return (
+    <tr className={rowClass}>
+      {data.map((item, index) => (
+        <td
+          key={index}
+          className="border-b border-slate-400 px-4 py-2"
+        >
+          {item}
+        </td>
+      ))}
+      <td className="border-b border-slate-400 px-4 py-2">
+        <div className="flex gap-2 items-center">
+          <ActionButton onClick={onUpdate}>
+            <FaEdit className="mr-[6px]" size={16} />
+            <p className="text-slate-900 font-semibold text-xs">Update</p>
+          </ActionButton>
+          <ActionButton onClick={onDelete}>
+            <FaTrash className="mr-[6px]" size={16} />
+            <p className="text-slate-900 font-semibold text-xs">Delete</p>
+          </ActionButton>
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+TableRowWithAction.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.node).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  isHeader: PropTypes.bool,
+  index: PropTypes.number,
+};
--- src/pages/Slicing.jsx
+++ src/pages/Slicing.jsx
@@ -3,6 +3,7 @@ import ActionButton from "../components/atoms/ActionButton";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import LoginForm from "../components/organism/LoginForm";
 import Table from "../components/organism/Table";
+import TableWithActions from "../components/organism/TableWithActions";
 
 const tableHeaders = ["No", "Nama Hotel", "Total Tagihan", "Status Tagihan"];
 
@@ -39,6 +40,56 @@ const tableData = [
   },
 ];
 
+const tableHeaders2 = [
+  "No",
+  "Item",
+  "Quantity",
+  "Harga / Unit",
+  "Jumlah Harga",
+];
+
+const tableData2 = [
+  {
+    No: "1",
+    Item: "Hand Towel",
+    Quantity: 2,
+    "Harga / Unit": 1000000,
+    "Jumlah Harga": 2000000,
+  },
+  {
+    No: "2",
+    Item: "Hand Sanitizer",
+    Quantity: 2,
+    "Harga / Unit": 500000,
+    "Jumlah Harga": 1000000,
+  },
+  {
+    No: "3",
+    Item: "Fruit Tea",
+    Quantity: 3,
+    "Harga / Unit": 10000,
+    "Jumlah Harga": 30000,
+  },
+];
+
+const tableHeaders3 = ["No", "Nama Hotel/Produk", "Actions"];
+
+const tableDataActions = [
+  ["1", "Hotel Indonesia"],
+  ["2", "Hotel Bali"],
+  ["3", "Hotel Lombok"],
+  ["4", "Hotel Surabaya"],
+  ["5", "Hotel Bandung"],
+];
+
+const handleUpdate = (row) => {
+  alert(`Update clicked for: ${row[1]}`);
+};
+
+const handleDelete = (row) => {
+  alert(`Delete clicked for: ${row[1]}`);
+};
+
 export default function Slicing() {
   return (
     <div className="p-16 flex flex-col gap-10">
@@ -60,6 +111,13 @@ export default function Slicing() {
         <LoginForm />
       </div>
       <Table headers={tableHeaders} data={tableData} />
+      <Table headers={tableHeaders2} data={tableData2} />
+      <TableWithActions
+        headers={tableHeaders3}
+        data={tableDataActions}
+        onUpdate={handleUpdate}
+        onDelete={handleDelete}
+      />
     </div>
   );
 }
